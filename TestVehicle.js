5cfd2ec3e3f47b0d9005c56e6b6f90540b5505d4
brosenbusch <8brosenbusch@stpatrick.org>
2017-08-31 14:28:01 -0500

    Finished Vehicle Module

--- TestVehicle.js
+++ TestVehicle.js
@@ -2,7 +2,13 @@ const Vehicle = require('./Vehicle.js');
 
 function main(){
     //make two vehicles and take them for a spin.
-let flemcar = new Vehicle(8,20,20,25,0)
+let flemcar = new Vehicle(8,20,20,25,0);
+
+let hoogadiboogadie = new Vehicle (8,10,10,100,0);
+console.log(flemcar.totalMiles());
+flemcar.drive(3);
+console.log(flemcar.totalMiles());
+console.log(flemcar.fuelLeft());
 
 
 
--- Vehicle.js
+++ Vehicle.js
@@ -4,47 +4,53 @@ let Vehicle = function(e,f,g, mpg,d){
   let gastank = g;
   let averageMPG = mpg;
   let milesDriven = d;
+
+
+  function totalMiles(){
+    return milesDriven;
+  }
+
+  function fuelLeft(){
+    return fuel;
+  }
+
+  function fill(percent){
+    fuel += percent;
+    if(fuel > 1){
+      fuel = 1;
+    }
+  }
+
+  function drive(hours){
+    let fuelburn = hours*engineEfficiency();
+    if(fuelburn<=fuel){
+      milesDriven += (hours*averageMPG);
+      fuel -= hours*engineEfficiency();
+    }
+    else{
+      milesDriven += fuel*averageMPG;
+      fuel = 0;
+    }
+  }
+
+  function engineEfficiency(){
+    if(engine = 4){
+      return 1;
+    }
+    else if(engine = 6){
+      return 1.2;
+    }
+    else if(engine = 8){
+      return 1.36;
+    }
+    else{
+      return null;
+      console.log("number of cylinders nonexistent")
+    }
+
+  }
+
+  return {totalMiles,fuelLeft,fill,drive};
 }
 
-function totalMiles(){
-  return milesDriven;
-}
-
-function fuelLeft(){
-  return fuel;
-}
-
-function fill(percent){
-  fuel += percent;
-  if(fuel > 1){
-    fuel = 1;
-  }
-}
-
-function drive(hours){
-  let fuelburn = hours*engineEfficiency();
-  if(fuelburn<=fuel){
-    milesDriven += (hours*averageMPG);
-    fuel -= hours*engineEfficiency();
-  }
-  else{
-    milesDriven += fuel*averageMPG;
-    fuel = 0;
-  }
-}
-
-function engineEffiiency(){
-  if(engine = 4){
-    return 1;
-  }
-  else if(engine = 6){
-    return 1.2;
-  }
-  else if(engine = 8){
-    return 1.36;
-  }
-  else{
-    return null;
-    console.log("number of cylinders nonexistent")
-  }
-}
+module.exports = Vehicle;
